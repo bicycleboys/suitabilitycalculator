5a58499ae02470b2d5ad1b132874c90a3a906e98
natek17 <45103282+natek17@users.noreply.github.com>
2019-10-08 12:26:14 -0500

    started work on a bicycle level of service calculator.
    very preliminary, just needed to get the calcualtion from latex to 
    somewhere usable

--- calculators/lts.js
+++ calculators/lts.js
@@ -75,7 +75,7 @@ function bikeLaneCalculate(o) {
     }
   }
 
-  
+
   if (o.blockage == 'rarely')
     blockageLTS = 1;
   else if (o.blockage == 'frequently')
@@ -104,4 +104,32 @@ function mixedTrafficCalculate(o) {
     }
   }
   return p;
+}
+
+//putting this here because I don't know where else to put it
+fucntion bicycleLevelOfService(o){
+  let wosstar; //$w_{os}^*$, adjusted width of shoulder
+  let wt; //$W_{t}$, space available for bikes
+  let wv;
+  let we;
+  let phva;
+  let sra;
+  let vma;
+  let blos;
+  if (o.curb) wosstar = max(0.0, o.wos-1.5);
+  else wosstar = o.wos;
+  if(o.ppk = 0.0) wt = o.wol + o.wbl + wosstar;
+  else wt = o.wol + o.wbl
+  if(o.devided || o.vm > 160) wv = wt;
+  else wv = wt * (2 - .005 * o.vm);
+  if(o.wbl + wosstar < 4.0) we = Math.max(wv-(10*o.ppk),0.0);
+  else we = Math.max(wv + o.wbl + wosstar - (20*o.ppk), 0.0);
+  if((o.vm*(1-.001*o.phv) < 200.0) && o.phv > 50.0) phva = 50.0;
+  else phva = o.phv;
+  sra = Math.max(21, o.sr);
+  vma = Math.max(o.vm, 4*o.nth);
+  blos = .760 + (-.005*Math.pow(we, 2)) + (.507*Math.ln(vma/(4*o.nth))) +
+  (.199*(1.1199*Math.ln(sra - 20) + .8103)*(1+.1038* Math.pow(phva, 2))) +
+  (7.066/Math.pow(o.pc, 2))
+  
 }
