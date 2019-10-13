94bc4130a65f754ab7e81ddc5ed461f8977b86a5
natek17 <45103282+natek17@users.noreply.github.com>
2019-10-13 11:43:55 -0500

    added plos calculator function

--- calculators/lts.js
+++ calculators/lts.js
@@ -119,7 +119,7 @@ fucntion bicycleLevelOfService(o){
   if (o.curb) wosstar = max(0.0, o.wos-1.5);
   else wosstar = o.wos;
   if(o.ppk = 0.0) wt = o.wol + o.wbl + wosstar;
-  else wt = o.wol + o.wbl
+  else wt = o.wol + o.wbl;
   if(o.devided || o.vm > 160) wv = wt;
   else wv = wt * (2 - .005 * o.vm);
   if(o.wbl + wosstar < 4.0) we = Math.max(wv-(10*o.ppk),0.0);
@@ -132,4 +132,24 @@ fucntion bicycleLevelOfService(o){
   (.199*(1.1199*Math.ln(sra - 20) + .8103)*(1+.1038* Math.pow(phva, 2))) +
   (7.066/Math.pow(o.pc, 2));
   return blos;
+}
+
+function pedestrianLevelOfService(o){
+  let wosstar;
+  let wt;
+  let wv;
+  let w1;
+  let plos
+  if (o.curb) wosstar = max(0.0, o.wos-1.5);
+  else wosstar = o.wos;
+  if(o.ppk = 0.0) wt = o.wol + o.wbl + wosstar;
+  else wt = o.wol + o.wbl;
+  if(o.devided || o.vm > 160) wv = wt;
+  else wv = wt * (2 - .005 * o.vm);
+  if(o.ppk < .25 || o.parkingStriped) w1 = o.wbl + wosstar;
+  else w1 = 10.0;
+  plos = 6.0468 + (-1.2276 * Math.ln(wv + .5 * w1 + 50 * o.ppk + o.wbuf * o.fb + o.waa * (6.0 - .3 * o.waa))) +
+  (.0091* o.vm/(4*o.nth)) + (4*Math.pow(o.sr/100, 2));
+  return plos;
+
 }
