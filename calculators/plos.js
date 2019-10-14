b5d19d6b87f4ce5bd4cc684ec2e2ab34e824ed11
Zak Sharp <macmaster195@gmail.com>
2019-10-13 19:24:25 -0500

    Separated out blos and plos into their own modules

---
+++ calculators/plos.js
@@ -0,0 +1,21 @@
+
+
+export function calculate(o) {
+    let wosstar;
+    let wt;
+    let wv;
+    let w1;
+    let plos
+    if (o.curb) wosstar = max(0.0, o.wos - 1.5);
+    else wosstar = o.wos;
+    if (o.ppk = 0.0) wt = o.wol + o.wbl + wosstar;
+    else wt = o.wol + o.wbl;
+    if (o.devided || o.vm > 160) wv = wt;
+    else wv = wt * (2 - .005 * o.vm);
+    if (o.ppk < .25 || o.parkingStriped) w1 = o.wbl + wosstar;
+    else w1 = 10.0;
+    plos = 6.0468 + (-1.2276 * Math.ln(wv + .5 * w1 + 50 * o.ppk + o.wbuf * o.fb + o.waa * (6.0 - .3 * o.waa))) +
+        (.0091 * o.vm / (4 * o.laneCount)) + (4 * Math.pow(o.sr / 100, 2));
+    return plos;
+
+}
