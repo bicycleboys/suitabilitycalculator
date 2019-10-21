1bc3b2d22e33710f6ac71183fc90de5fe13f3dff
Zak Sharp <macmaster195@gmail.com>
2019-10-21 16:27:24 -0500

    added html files for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'build'),
-  },
+    entry: './src/main.js',
+    mode: 'development',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'build'),
+    },
     optimization: {
-		// We no not want to minimize our code.
-		minimize: false
-	},
+        // We no not want to minimize our code.
+        minimize: false
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ //add index.html to build
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new HtmlWebpackPlugin({ //add form.html to build
+            template: './src/form.html',
+            filename: 'form.html' 
+        })
+    ]
 };
