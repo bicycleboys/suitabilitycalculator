1e3c8759c85eeeefba34ebe4660098b387de0a0e
natek17 <45103282+natek17@users.noreply.github.com>
2019-11-10 11:39:42 -0600

    fbdao is cooperating now

--- src/daos/fbdao.js
+++ src/daos/fbdao.js
@@ -5,58 +5,6 @@ import 'firebase/auth';
 export class fbdao{
 
   constructor() {
-    // var db;
-    // var firebaseConfig = {
-    //     apiKey: "AIzaSyChkACWp5aGd0s3ovbD7sRMugbSaljjyZU",
-    //     authDomain: "bicycleboys.firebaseapp.com",
-    //     databaseURL: "https://bicycleboys.firebaseio.com",
-    //     projectId: "bicycleboys",
-    //     storageBucket: "bicycleboys.appspot.com",
-    //     messagingSenderId: "691023297022",
-    //     appId: "1:691023297022:web:91ea734781e0029e99a8af",
-    //     measurementId: "G-ZRQ1KHK7PL"
-    //   };
-    // try{
-    //   var app = firebase.initializeApp(firebaseConfig);
-    //   firebase.firestore(app).enablePersistence().catch(function(err) {
-    //         if (err.code == 'failed-precondition') {
-    //             console.log("failed precondition");
-    //         } else if (err.code == 'unimplemented') {
-    //           console.log("unimplemented");
-    //         }
-    //     });
-    //     //set cache size to be as big as we need
-    //   firebase.firestore(app).settings({
-    //       cacheSizeBytes: firebase.firestore.CACHE_SIZE_UNLIMITED
-    //     });
-    // }catch{
-    //   console.log("Firebase already intinalized");
-    // }
-    // var db = firebase.firestore(app);
-    // this.db = db;
-    // db.collection("users").add({
-    //   first: "Ada",
-    //   last: "Lovelace",
-    //   born: 1815
-    // })
-    // .then(function(docRef) {
-    //   console.log("Document written with ID: ", docRef.id);
-    // })
-    // .catch(function(error) {
-    //   console.error("Error adding document: ", error);
-    // });
-    //
-    // db.collection("cities").doc("LA").set({
-    //   name: "Los Angeles",
-    //   state: "CA",
-    //   country: "USA"
-    // })
-    // .then(function() {
-    //   console.log("Document successfully written!");
-    // })
-    // .catch(function(error) {
-    //   console.error("Error writing document: ", error);
-    // });
     var firebaseConfig = {
       apiKey: "AIzaSyChkACWp5aGd0s3ovbD7sRMugbSaljjyZU",
       authDomain: "bicycleboys.firebaseapp.com",
