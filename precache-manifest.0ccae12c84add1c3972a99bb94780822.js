95ffb4ff292de88ff3b6db6698403477662e6e9c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-18 22:51:47 +0000

    Deploy bicycleboys/suitabilitycalculator to github.com/bicycleboys/suitabilitycalculator.git:gh-pages

---
+++ precache-manifest.0ccae12c84add1c3972a99bb94780822.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a55b8b2de5715d28a44c1507cef7e0",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "a523c23870fa250c7c89e8e173c1fba2",
+    "url": "form.html"
+  },
+  {
+    "revision": "24aea6c5a0474b542a217090a8da6428",
+    "url": "index.html"
+  },
+  {
+    "revision": "e7e13387010e2ad00295",
+    "url": "main.js"
+  }
+]);
