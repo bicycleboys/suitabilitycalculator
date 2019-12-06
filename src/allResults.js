76c65a3968f49bde581e09cc3c3f4f1f98446455
natek17 <45103282+natek17@users.noreply.github.com>
2019-12-06 13:30:06 -0600

    working on making the table excel friendly

--- src/allResults.js
+++ src/allResults.js
@@ -9,7 +9,7 @@ function getListAndPopulate(dao, table){
   var list = dao.getList()
   return list.then(function (l) {
     console.log(Object.values(list));
-    let data = Object.keys(l[0]);
+    let data = Object.values(l[0]);
     console.log(l);
     console.log(Object.entries(l));
     console.log(typeof l);
@@ -25,63 +25,138 @@ function generateTableHead(table, data) {
   let thead = table.createTHead();
   let row = thead.insertRow();
   for (let key of data) {
-    let th = document.createElement("th");
-    let text = document.createTextNode(key);
-    th.appendChild(text);
-    row.appendChild(th);
+    console.log(key);
+    let th;
+    let text;
+
+    for (let i in Object.values(key)){
+      if (typeof Object.values(key)[i] === 'undefined'){
+        th = document.createElement("th");
+        text = document.createTextNode(" ");
+        th.appendChild(text);
+        row.appendChild(th);
+        console.log("Hit undefined");
+        //ntext = document.createTextNode( ntext.wholeText + " " + element[key]);
+      }
+      else{
+        let array = Object.values(key)[i];
+        //ntext = document.createTextNode( ntext.wholeText+ ' ' + printObject(array, document, row));
+        for(let j in Object.values(array)){
+          if (isNaN(Object.keys(array)[j])){
+          th = document.createElement("th");
+          text = document.createTextNode(Object.keys(array)[j]);
+          th.appendChild(text);
+          row.appendChild(th);
+        }
+        }
+      }
+    }
+    if('RLCount' === Object.keys(key)[0]){
+      console.log("Into RLCount");
+      //ntext = document.createTextNode( ntext.wholeText+ ' ' + printObject(element[key], document, row));
+      for (let k in Object.keys(Object.values(key))){
+        th = document.createElement("th");
+        text = document.createTextNode(Object.keys(key)[k]);
+        th.appendChild(text);
+        row.appendChild(th);
+      }
+    }
+    else if ('seconds' === Object.keys(key)[0]) {
+      th = document.createElement("th");
+      text = document.createTextNode("Timestamp");
+      th.appendChild(text);
+      row.appendChild(th);
+    }
+    else{
+      console.log("Oops, wrote the conditions wrong");
+    }
   }
+
+
+  /*
+  for (let i in Object.values(key)){
+  if(!(typeof data[key] === 'undefined'||typeof data[key][i] === 'undefined')){
+  let array = data[key][i];
+  for(let e in Object.values(array)){
+  console.log(Object.values(array)[e]);
+  let th = document.createElement("th");
+  let text = document.createTextNode(key[i]);
+  th.appendChild(text);
+  row.appendChild(th);
+}
+}
+else{
+let th = document.createElement("th");
+let text = document.createTextNode(key[i]);
+th.appendChild(text);
+row.appendChild(th);
+}
+}
+*/
 }
 
 function generateTable(table, data) {
   for (let element of data) {
     let row = table.insertRow();
     for (let key in element) {
-      let cell = row.insertCell();
       let text = document.createTextNode(element[key]);
       let ntext = document.createTextNode("");
       //ntext = document.createTextNode( ntext.wholeText + " " + element[key]);
-      console.log(element[key]);
       for (let i in Object.values(key)){
         if (typeof element[key][i] === 'undefined'){
-          console.log("Hit undefined");
-          //ntext = document.createTextNode( ntext.wholeText + " " + element[key]);
+          //let cell = row.insertCell();
+          //ntext = document.createTextNode(" ");
+          //cell.appendChild(ntext);
+          //console.log("Hit undefined");
         }
         else{
           let array = element[key][i];
-          console.log(array);
-          console.log(Object.values(array));
-          ntext = document.createTextNode( ntext.wholeText+ ' ' + printObject(array));
-          for(let e in Object.values(array)){
-            console.log(Object.values(array)[e]);
-            //ntext = document.createTextNode(ntext.wholeText + " " + Object.values(array)[e]);
-          }
+          //console.log(array);
+          //console.log(Object.values(array));
+          //ntext = document.createTextNode( ntext.wholeText+ ' ' + printObject(array, document, row));
+          printObject(array, document, row);
         }
       }
       if('RLCount' in element[key]){
-        console.log("Into if block");
-        console.log(element[key]);
-        ntext = document.createTextNode( ntext.wholeText+ ' ' + printObject(element[key]));
+        //ntext = document.createTextNode( ntext.wholeText+ ' ' + printObject(element[key], document, row));
+        printObject(element[key], document, row);
       }
       else if ('seconds' in element[key]) {
+        let cell = row.insertCell();
         ntext = document.createTextNode( ntext.wholeText + " " + element[key]);
+        cell.appendChild(ntext);
       }
 
-      cell.appendChild(ntext);
+
     }
 
   }
 }
 
-function printObject(obj){
-  var output = '';
-  for (var property in obj) {
-    if(!(Number.isNaN(obj[property]) || obj[property] === "NaN" || obj[property] === " " || obj[property] == "")){
-      output += property + ': ' + obj[property]+'; \n';
-    }
+function printObject(obj, document, row){
+  console.log(obj);
+  for (let property in obj) {
+    //if(!(Number.isNaN(obj[property]) || obj[property] === "NaN" || obj[property] === " " || obj[property] == "")){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj[property]);
+    cell.appendChild(ntext);
+    //}
   }
-  console.log(output);
-  return output;
+  //console.log(output);
+  return;
 }
+
+//The old, working but ugly print object
+//function printObject(obj){
+//  var output = '';
+//  for (var property in obj) {
+//    if(!(Number.isNaN(obj[property]) || obj[property] === "NaN" || obj[property] === " " || obj[property] == "")){
+//      output += property + ': ' + obj[property]+'; \n';
+//    }
+//  }
+//  console.log(output);
+//  return output;
+//}
 
 function isIterable(obj) {
   // checks for null and undefined
