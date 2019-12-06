00c9323bf905b6fed540c59808b343766f987060
natek17 <45103282+natek17@users.noreply.github.com>
2019-12-06 15:53:17 -0600

    hacked together, but the table works for variable input. it's pretty ugly tho if we add any more data fields we have to add them into the table too

--- src/allResults.js
+++ src/allResults.js
@@ -1,4 +1,5 @@
 import {FBDao} from './daos/fbdao';
+import calculatorUtils from './calculators/calculatorUtils'
 import "./styles.css";
 
 var dao = new FBDao();
@@ -43,12 +44,20 @@ function generateTableHead(table, data) {
         //ntext = document.createTextNode( ntext.wholeText+ ' ' + printObject(array, document, row));
         for(let j in Object.values(array)){
           if (isNaN(Object.keys(array)[j])){
-          th = document.createElement("th");
-          text = document.createTextNode(Object.keys(array)[j]);
-          th.appendChild(text);
-          row.appendChild(th);
+            if(Object.keys(array)[j] === "grade"){
+
+              th = document.createElement("th");
+              text = document.createTextNode("Because");
+              th.appendChild(text);
+              row.appendChild(th);
+            }
+            th = document.createElement("th");
+            text = document.createTextNode(Object.keys(array)[j]);
+            th.appendChild(text);
+            row.appendChild(th);
+          }
         }
-        }
+
       }
     }
     if('RLCount' === Object.keys(key)[0]){
@@ -114,7 +123,8 @@ function generateTable(table, data) {
           //console.log(array);
           //console.log(Object.values(array));
           //ntext = document.createTextNode( ntext.wholeText+ ' ' + printObject(array, document, row));
-          printObject(array, document, row);
+          console.log(array);
+          printArrayObject(array, document, row);
         }
       }
       if('RLCount' in element[key]){
@@ -134,18 +144,368 @@ function generateTable(table, data) {
 }
 
 function printObject(obj, document, row){
-  console.log(obj);
-  for (let property in obj) {
+  //console.log(obj);
+  //this for loop doesn't handle when an object has a property that another doesn't
+  //it results in an uneven table
+  /*for (let property in obj) {
     //if(!(Number.isNaN(obj[property]) || obj[property] === "NaN" || obj[property] === " " || obj[property] == "")){
     let cell = row.insertCell();
     let ntext = document.createTextNode(obj[property]);
     cell.appendChild(ntext);
     //}
+  }*/
+
+  if("RLCount" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["RLCount"]);
+    cell.appendChild(ntext);
   }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("RLLength" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["RLLength"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("adt" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["adt"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("blockage" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["blockage"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("centerline" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["centerline"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("curb" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["curb"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("fb" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["fb"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("laneCount" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["laneCount"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("laneShift" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["laneShift"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("laneWidth" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["laneWidth"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("lanesCombinedWidth" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["lanesCombinedWidth"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("median" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["median"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("parkingWidth" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["parkingWidth"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("pc" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["pc"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("phv" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["phv"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("ppk" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["ppk"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("runningSpeed" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["runningSpeed"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("segmentBetween1" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["segmentBetween1"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("segmentBetween2" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["segmentBetween2"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("segmentName" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["segmentName"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("segmentType" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["segmentType"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("speed" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["speed"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("totalLanes" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["totalLanes"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("turningSpeed" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["turningSpeed"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("waa" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["waa"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("wbl" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["wbl"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("wbuf" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["wbuf"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }if("wol" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["wol"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("wos" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["wos"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("xStreetWidth" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["xStreetWidth"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+
   //console.log(output);
   return;
 }
+function printArrayObject(obj, document, row){
+  console.log(obj);
+  //this is disgusting and it hurt to write, but
+  //I can't get the for loop to work for the life of me
+  if("because" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["because"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("grade" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["grade"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("name" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["name"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
+  if("points" in obj){
+    let cell = row.insertCell();
+    let ntext = document.createTextNode(obj["points"]);
+    cell.appendChild(ntext);
+  }
+  else{
+    let cell = row.insertCell();
+    let ntext = document.createTextNode("");
+    cell.appendChild(ntext);
+  }
 
+  //console.log(output);
+  return;
+}
 //The old, working but ugly print object
 //function printObject(obj){
 //  var output = '';
