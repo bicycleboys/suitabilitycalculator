1b6e2756d34db8c29f6f844535b6b109931015d4
Zak Sharp <macmaster195@gmail.com>
2020-11-29 17:12:25 -0600

    code formatter

--- src/calculators/blos.ts
+++ src/calculators/blos.ts
@@ -1,23 +1,21 @@
-export function calculate(o:SegmentDataObject):CalculatorResponse {
-    let properties:NotCalculated|null = requireProperties(o,'adt','ppk','curb','wos','wol','devided', 'wbl', 'wos', 'runningSpeed','laneCount','phv','pc')
-    if(properties != null) return properties;
-    let wosstar; //$w_{os}^*$, adjusted width of shoulder
-    let wt; //$W_{t}$, space available for bikes
+export function calculate(o: SegmentDataObject): CalculatorResponse {
+    let properties: NotCalculated | null = requireProperties(o, 'adt', 'ppk', 'curb', 'wos', 'wol', 'devided', 'wbl', 'wos', 'runningSpeed', 'laneCount', 'phv', 'pc')
+    if (properties != null) return properties;
     let wv;
     let we;
     let phva;
     let sra;
     let vma;
     let blos;
-    let vm;
     let appk;
     let c1, c2, c3, c4;
-    let ppk = o.ppk/100;
-    vm = o.adt / 20;
-    if (o.curb) wosstar = Math.max(0.0, o.wos - 1.5);
-    else wosstar = o.wos;
-    if (ppk == 0.0) wt = o.wol + o.wbl + wosstar;
-    else wt = o.wol + o.wbl;
+    let ppk = o.ppk / 100;
+
+    const vm = o.adt / 20;
+
+    const wosstar = (o.curb) ? Math.max(0.0, o.wos - 1.5) : o.wos; //$w_{os}^*$, adjusted width of shoulder
+
+    const wt = (ppk == 0.0) ? o.wol + o.wbl + wosstar : o.wol + o.wbl; //$W_{t}$, space available for bikes
     if (o.median || vm > 160) wv = wt;
     else wv = wt * (2 - .005 * vm);
     if (o.wbl + o.wos < 4.0) we = Math.max(wv - (10 * ppk), 0.0);
@@ -28,34 +26,35 @@ export function calculate(o:SegmentDataObject):CalculatorResponse {
     vma = Math.max(vm, 4 * o.laneCount);
     c1 = (-.005 * Math.pow(we, 2));
     c2 = (.507 * Math.log(vma / (4 * o.laneCount)));
-    c3 = (.199 * (1.1199 * Math.log(sra - 20) + .8103) * (Math.pow(1 + .1038 *phva, 2)));
+    c3 = (.199 * (1.1199 * Math.log(sra - 20) + .8103) * (Math.pow(1 + .1038 * phva, 2)));
     c4 = (7.066 / Math.pow(o.pc, 2));
-    blos = ((.760) + c1 + c2 +c3 +c4).toFixed(2);
+    blos = ((.760) + c1 + c2 + c3 + c4).toFixed(2);
     blos = parseFloat(blos);
 
-    if(isNaN(blos)){
-        return {name: "BLOS", because: ""}
+    if (isNaN(blos)) {
+        return { name: "BLOS", because: "" }
     }
 
     let grade;
-    if(blos<=2){
-        grade="A"
-    }else if(blos<=2.75){
-        grade="B"
-    }else if(blos<=3.5){
-        grade="C"
-    }else if(blos<=4.25){
-        grade="D"
-    }else if(blos<=5){
-        grade="E"
-    }else grade="F"
-    return {points:blos, grade: grade, name: "BLOS"};
+    if (blos <= 2) {
+        grade = "A"
+    } else if (blos <= 2.75) {
+        grade = "B"
+    } else if (blos <= 3.5) {
+        grade = "C"
+    } else if (blos <= 4.25) {
+        grade = "D"
+    } else if (blos <= 5) {
+        grade = "E"
+    } else grade = "F"
+
+    return { points: blos, grade: grade, name: "BLOS" };
 }
 
-function requireProperties(o:Object,...properties:string[]):NotCalculated|null{
-    properties.forEach(p => {
+function requireProperties(o: Object, ...properties: string[]): NotCalculated | null {
+    for (var p of properties) {
         if (!(p in o))
-            return {name:"BLOS", because: p}
-    });
+            return { name: "BLOS", because: p }
+    }
     return null;
 }
